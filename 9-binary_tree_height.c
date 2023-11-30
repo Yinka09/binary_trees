4ce35166300c337a174f8ee309667e4213e8171d
Isoken <isokenjune@gmail.com>
2023-11-30 07:36:01 -0800

    number 9 correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,8 +3,6 @@
 /**
  * binary_tree_height - Computes the height of a binary tree
  * @tree: pointer to the root node of the tree to measure
- * @func:  pointer to the function to call for each node
- * @height: pointer to the variable that will contain the height of the tree
  * Return: height of the tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
